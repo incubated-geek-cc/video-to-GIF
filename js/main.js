2bd1c9217fb2c68f7762f3bc0436a9fa50f2e4fa
incubated-geek-cc <xuemanxu.cc@gmail.com>
2022-04-20 18:15:37 +0800

    Debugged the frame callbacks

--- js/main.js
+++ js/main.js
@@ -27,13 +27,9 @@ var inputVideoClipFileBtn=document.getElementById('inputVideoClipFileBtn');
 var inputVideoDetails = document.getElementById('inputVideoDetails');
 var inputVideoPreview = document.getElementById('inputVideoPreview');
 
-var bitmap = document.createElement('canvas');
-var videoObj = document.createElement('video');
-var vidDuration=0;
-var displayedHeight=500;
-var displayedWidth;
 inputVideoClipFileBtn.addEventListener('click', () => {
-	inputVideoClipFile.click();
+	let clickEvent = new MouseEvent('click', { view: window, bubbles: false, cancelable: false });
+    inputVideoClipFile.dispatchEvent(clickEvent);
 });
 
 var loadingBar=document.getElementById('loadingBar');
@@ -51,157 +47,180 @@ const loadProgressMapper = [
     '▮▮▮▮▮▮▮▮▮▮'
 ];
 const byteToKBScale = 0.0009765625;
-var counter=0;
+const displayedSize=500;
 
-var fps=60;
-var frameInterval=165;
+var continueCallback=true;
+// window.devicePixelRatio = 3.0;
+var scale = window.devicePixelRatio;
+var requiredFPS=10;
+var fps;
 
-inputVideoClipFile.onchange = function(uploadFle) {
-    if(uploadFle.target.value !== '' && uploadFle.target.files.length>0) {
-        if (!window.FileReader) {
-            alert('Your browser does not support HTML5 "FileReader" function required to open a file.');
-        } else {
-        	framesToRemove={};
-            let file = inputVideoClipFile.files[0];
+function scaleCanvas(_CANVAS, videoObj, vidHeight, vidWidth, scale) {
+    _CANVAS['style']['height'] = `${vidHeight}px`;
+    _CANVAS['style']['width'] = `${vidWidth}px`;
 
-            let fileName=file.name;
-            let fileSize=(file.size/1024).toFixed(2);
-            let fileType=file.type;
+    let cWidth=vidWidth*scale;
+    let cHeight=vidHeight*scale;
 
-            let fileredr = new FileReader();
-            fileredr.onload = function (fle) {
-                var b64Str=fle.target.result;
-                
-                if(videoObj.canPlayType(fileType)) {
-                	videoObj.id='inputVideo';
-					videoObj.src=b64Str;
-					videoObj.height=displayedHeight;
-				}
-				inputVideoPreview.insertAdjacentHTML('beforeend',`${videoObj.outerHTML}`);
-                videoObj.oncanplay = () => {
-                	var toContinueLoading=setInterval( () => {
-                		var toDisplay=`${loadProgressMapper[counter++]}	ʟᴏᴀᴅɪɴɢ`;
-					    loadingBar.innerHTML=toDisplay;
-					    if(counter === loadProgressMapper.length) { 
-					    	counter=0;
-					    }
-                	}, 1000/fps);
+     _CANVAS.width=cWidth;
+    _CANVAS.height=cHeight;
 
-					vidDuration=parseInt(videoObj.duration);
+    _CANVAS.getContext('2d').scale(scale, scale);
+}
 
-					var vidHeight=videoObj.videoHeight;
-					var vidWidth=videoObj.videoWidth;
-					displayedWidth=((displayedHeight/vidHeight)*vidWidth);
+function readFileAsDataURL(file) {
+    return new Promise((resolve,reject) => {
+        let fileredr = new FileReader();
+        fileredr.onload = () => resolve(fileredr.result);
+        fileredr.onerror = () => reject(fileredr);
+        fileredr.readAsDataURL(file);
+    });
+}
+// oncanplay
+const loadVideo = (url) => new Promise((resolve, reject) => {
+    var vid = document.createElement('video');
+    vid.addEventListener('canplay', () => resolve(vid));
+    vid.addEventListener('error', (err) => reject(err));
+    vid.src = url;
+});
+inputVideoClipFile.addEventListener('change', async(evt) => {
+	if (!window.FileReader) { alert('Your browser does not support HTML5 "FileReader" function required to open a file.'); return; } 
+	let file = evt.target.files[0];
+    if(!file) return;
+    framesToRemove={};
 
-					let videoDetails='<tr><td>ℹ '+[
-    					`File Name: <b>${fileName}</b>`,
-    					`Type: <b>${fileType}</b>`, 
-    					`Size: <b>${fileSize} ㎅</b>`,
-    					`Frame(ᴡ ⨯ ʜ): <b>${vidWidth} ᵖˣ ⨯ ${vidHeight} ᵖˣ</b>`,
-    					`Length: <b>00:00:${ vidDuration >=10 ? vidDuration : ('0'+vidDuration)}</b>`
-					].join(' │ ') +'</td></tr>';
+    let fileName=file.name;
+    let fileType=file.type;
+    let fileSize=(file.size/1024).toFixed(2);
 
-					inputVideoDetails.innerHTML=videoDetails;
+	let b64Str = await readFileAsDataURL(file);
+	let videoObj=await loadVideo(b64Str);
+	videoObj.autoplay=false;
+	videoObj.muted=true;
+	videoObj.loop=false;
 
-					bitmap.id='bitmap';
-					bitmap.width=vidWidth;
-		            bitmap.height=vidHeight;
+	let vidDuration=videoObj.duration;
+	let vidHeight=videoObj.videoHeight; // 720
+	let vidWidth=videoObj.videoWidth; // 1280
 
-		            inputVideoPreview.insertAdjacentHTML('beforeend', `<div style="display:none">${bitmap.outerHTML}</div>`);
+	videoObj.height=vidHeight;
+	videoObj.width=vidWidth;
+	videoObj['style']['height']=`${vidHeight}px`;
+	videoObj['style']['width']=`${vidWidth}px`;
 
-		            const inputVideo=document.getElementById('inputVideo');
-		            const bitmapCanvas=document.getElementById('bitmap');
-		            const bitmapCtx = bitmapCanvas.getContext('2d');
+	document.getElementById('hiddenCanvas').appendChild(videoObj);
 
-		            const background = async() => {
-		               bitmapCtx.fillStyle = '#FFFFFF';
-		               bitmapCtx.fillRect(0, 0, vidWidth, vidHeight);
-		               return 'success';
-		            };
+	let videoDetails='<tr><td>ℹ '+[
+		`File Name: <b>${fileName}</b>`,
+		`Type: <b>${fileType}</b>`, 
+		`Size: <b>${fileSize} ㎅</b>`,
+		`Frame(ᴡ ⨯ ʜ): <b>${vidWidth} ᵖˣ ⨯ ${vidHeight} ᵖˣ</b>`,
+		`Length: <b>00:00:${ vidDuration >=10 ? vidDuration : ('0'+vidDuration)}</b>`
+	].join(' │ ') +'</td></tr>';
+	inputVideoDetails.innerHTML=videoDetails;
 
-		            const encoder = new GIFEncoder(vidWidth, vidHeight);
-		            encoder.setRepeat(0);
-		        	encoder.setDelay(frameInterval);
-	        		encoder.setQuality(10);
-
-		            let staticFrames='';
-		            let frameIndex=0;
-
-		            inputVideo.muted = true;
-					inputVideo.loop = false;
-					inputVideo.autoplay=true;
-
-					var continueAddFrame=true;
-
-					var frameB64Str='';
-					const step = async() => {
-						let bgStatus=await background();
-						await new Promise(resolve => {
-					        bitmapCtx.drawImage(inputVideo, 0, 0, vidWidth, vidHeight);
-					        frameB64Str=bitmapCanvas.toDataURL();
-					        if(Object.keys(framesToRemove).length===0) {
-					        	framesToRemove[frameB64Str]=true;
-					        }
-					        if(!framesToRemove[frameB64Str]) {
-					        	if(continueAddFrame) {
-				        			encoder.addFrame(bitmapCtx);
-				        		}
-				        	}
-				        	resolve();
-					    });
-					    if(!framesToRemove[frameB64Str]) {
-						    staticFrames+=`<th><small>Frame #${frameIndex++}</small><br><img src=${frameB64Str} width='75' /></th>`;
-						}
-					    window.requestAnimationFrame(step);
-					};
-		        	inputVideo.addEventListener('play', () => {
-		        		encoder.start();
-		        		step();
-					  	window.requestAnimationFrame(step);
-		        	});
-		        	inputVideo.addEventListener('ended', () => {
-		        		clearInterval(toContinueLoading);
-		        		continueAddFrame=false;
-		        		loadingBar.innerHTML='';
-					  	encoder.finish();
-
-			            var fileType='image/gif';
-			            var fileName = `gif-output-${(new Date().toGMTString().replace(/(\s|,|:)/g,''))}.gif`;
-			            var readableStream=encoder.stream();
-			            var binary_gif =readableStream.getData();
-						var b64Str = 'data:'+fileType+';base64,'+encode64(binary_gif);
-						var fileSize = readableStream.bin.length*byteToKBScale;
-						fileSize=fileSize.toFixed(2);
-
-						let dwnlnk = document.createElement('a');
-						dwnlnk.download = fileName;
-						dwnlnk.innerHTML = `💾 <small>Save</small>`;
-						dwnlnk.className = 'btn btn-outline-dark';
-						dwnlnk.href = b64Str;
-
-						let htmlStr='';
-			            htmlStr+='<thead>';
-			            htmlStr+='<tr align="left">'; 
-			            htmlStr+=`<th colspan='2' class='imageBg'><h4>🎬 Output GIF</h4><img id='outputGif' src='${b64Str}' alt='${fileName}' /><div class='table-responsive'><table><tr align='center'>${staticFrames}</tr></table></div></th>`;
-			            htmlStr+='</tr>';
-			            htmlStr+='</thead>';
-
-			            htmlStr+='<tbody>';
-			            htmlStr+='<tr>';
-			            htmlStr+='<td>ℹ '+[
-			            					`Type: <b>${fileType}</b>`, 
-			            					`Size: <b>${fileSize} ㎅</b>`, 
-			            					`# of Frame(s): <b>${frameIndex}</b>`,
-			            					`Frame (ᴡ ⨯ ʜ): <b>${ parseInt(vidWidth)} ᵖˣ ⨯ ${vidHeight} ᵖˣ</b>`,
-			            					`${dwnlnk.outerHTML}`
-			        					].join(' │ ') +'</td>'; 
-			            htmlStr+='</tr>';
-			            htmlStr+='</tbody>';
-			            inputVideoDetails.insertAdjacentHTML('afterend', htmlStr);
-		        	});
-                };
-           } // end file-reader onload
-           fileredr.readAsDataURL(file);
-        }; // end if-else
+	let _CANVAS = document.createElement('canvas');
+	scaleCanvas(_CANVAS, videoObj, vidHeight, vidWidth, scale);
+	inputVideoPreview.appendChild(_CANVAS);
+    // =============== calculate displayed sizes ====================
+    let sizeBenchmark=vidHeight;
+    if(vidWidth>vidHeight) {
+    	sizeBenchmark=vidWidth;
     }
-};
+	let scaleRatio=parseFloat(displayedSize/sizeBenchmark);
+	let displayedHeight=scaleRatio*vidHeight;
+	let displayedWidth=scaleRatio*vidWidth;
+
+	videoObj['style']['height']=`${displayedHeight}px`;
+	videoObj['style']['width']=`${displayedWidth}px`;
+
+	scaleCanvas(_CANVAS, videoObj, displayedHeight, displayedWidth, scale);
+
+	var encoder = new GIFEncoder(vidWidth, vidHeight);
+    encoder.setRepeat(0);
+	encoder.setDelay(0);
+	encoder.setQuality(10);
+
+	var startTime=0.0;
+	var frameIndex=0;
+	var staticFrames='';
+
+	const step = async() => {
+		await new Promise(resolve => {
+			if(startTime == 0.0) startTime=(Date.now()); // in milliseconds
+			
+			let _CANVAS_CTX=_CANVAS.getContext('2d');
+	      	_CANVAS_CTX.drawImage(videoObj, 0, 0, displayedWidth, displayedHeight);
+	      	encoder.addFrame(_CANVAS_CTX);
+
+	      	let frameB64Str=_CANVAS.toDataURL();
+	      	staticFrames+=`<th><small>Frame #${frameIndex++}</small><br><img src=${frameB64Str} width='75' /></th>`;
+
+	      	let elapsed = ((Date.now()) - startTime) / 1000.0;
+	      	fps = (frameIndex / elapsed);
+
+	      	resolve();
+      	});
+      	if(continueCallback) {
+      		videoObj.requestVideoFrameCallback(step);
+      	}
+    };
+
+	videoObj.addEventListener('play', (vEvt) => {
+		if(continueCallback) {
+			videoObj.requestVideoFrameCallback(step);
+		}
+		encoder.start();
+		loadingBar['style']['display']='block';
+		loadingBar['style']['background-image']='url(img/loading.gif)';
+	    loadingBar['style']['background-position']='center';
+	    loadingBar['style']['background-repeat']='no-repeat';
+	    loadingBar['style']['background-size']='contain';
+
+	    loadingBar['style']['height']='2rem';
+	    loadingBar['style']['width']='19rem';
+	}, false);
+	
+	videoObj.addEventListener('ended', (vEvt) => {
+		loadingBar['style']['display']='none';
+		continueCallback=false;
+		encoder.finish();
+
+    	var fileType='image/gif';
+        var fileName = `gif-output-${(new Date().toGMTString().replace(/(\s|,|:)/g,''))}.gif`;
+        var readableStream=encoder.stream();
+        var binary_gif =readableStream.getData();
+		var b64Str = 'data:'+fileType+';base64,'+encode64(binary_gif);
+		var fileSize = readableStream.bin.length*byteToKBScale;
+		fileSize=fileSize.toFixed(2);
+
+		let dwnlnk = document.createElement('a');
+		dwnlnk.download = fileName;
+		dwnlnk.innerHTML = `💾 <small>Save</small>`;
+		dwnlnk.className = 'btn btn-outline-dark';
+		dwnlnk.href = b64Str;
+
+		let htmlStr='';
+        htmlStr+='<thead>';
+        htmlStr+='<tr align="left">'; 
+        htmlStr+=`<th colspan='2' class='imageBg'><h4>🎬 Output GIF</h4><img id='outputGif' src='${b64Str}' style='width:${displayedWidth}px;height:${displayedHeight}px;' alt='${fileName}' /><div class='table-responsive'><table><tr align='center'>${staticFrames}</tr></table></div></th>`;
+        htmlStr+='</tr>';
+        htmlStr+='</thead>';
+
+        htmlStr+='<tbody>';
+        htmlStr+='<tr>';
+        htmlStr+='<td>ℹ '+[
+        					`Type: <b>${fileType}</b>`, 
+        					`Size: <b>${fileSize} ㎅</b>`, 
+        					`# of Frame(s): <b>${frameIndex}</b>`,
+        					`FPS: <b>${fps.toFixed(2)}</b>`, 
+        					`Frame (ᴡ ⨯ ʜ): <b>${ parseInt(vidWidth)} ᵖˣ ⨯ ${vidHeight} ᵖˣ</b>`,
+        					`${dwnlnk.outerHTML}`
+    					].join(' │ ') +'</td>'; 
+        htmlStr+='</tr>';
+        htmlStr+='</tbody>';
+        inputVideoDetails.insertAdjacentHTML('afterend', htmlStr);
+	}, false);
+
+	videoObj.play();
+});
